83083c504fba72c41f6aca432767af9586f81f30
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-04-18 14:26:28 -0400

    initial inquirer .then function

--- app.js
+++ app.js
@@ -84,4 +84,15 @@ inquirer.prompt([
             return answers.position === "Intern";
         }
     },
-])
+]).then(function(data) {
+    
+    fs.writeFile("./output/teamsite.html", render, function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+    
+        console.log("Success!");
+    
+      });
+});
